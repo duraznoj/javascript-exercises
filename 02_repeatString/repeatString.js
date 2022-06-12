81067119259602a50f59a9b5e1f1a9f6603e220d
duraznoj <sashka.warner@gmail.com>
2022-06-11 20:12:40 -0700

    Create repeatString function and fix bugs

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+//let outVar
+const repeatString = function(string, num) {
+  let outVar = "";
+  if(num < 0) {
+    outVar = "ERROR";
+    return outVar;
 
+  } else {
+    for(let i = 0; i <= num-1; i++) {
+      outVar += string;
+    }
+    return outVar;
+
+  }
+  
 };
 
 // Do not edit below this line
