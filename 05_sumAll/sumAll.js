e27c98a00af22b95a0a4139302bad158ef010249
duraznoj <sashka.warner@gmail.com>
2022-06-11 20:48:19 -0700

    Create sumAll function and fix bugs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+  if(int1 >= 0 && int2 >= 0 && typeof int1 === "number" && typeof int2 === "number") {
+    let sortedArr = [int1, int2].sort()
+    let outSum = 0;
+    for(let i = sortedArr[0]; i <= sortedArr[1]; i++) {
+      outSum += i;
+    }
+    return outSum;
+  } else {
+    return "ERROR";
+  }
+  
 };
 
 // Do not edit below this line
