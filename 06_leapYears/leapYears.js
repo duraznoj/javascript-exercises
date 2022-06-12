adcdd353aff1d0024f0faaf791b6ba8fcdc0f2c7
duraznoj <sashka.warner@gmail.com>
2022-06-11 21:07:38 -0700

    Create leapYears function and fix bugs

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(inYear) {
+  if(inYear % 4 === 0) {
+    if(inYear % 100 === 0 && inYear % 400 !== 0) {
+      //not leap year
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
