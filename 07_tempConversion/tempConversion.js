f9b46e305ec29767106296e71f78771ff52f74b7
duraznoj <sashka.warner@gmail.com>
2022-06-11 21:17:18 -0700

    Create tempConversion function and fix bugs

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+//Formula	
+//(0°C × 9/5) + 32 = 32°F
 
+const ftoc = function(inTempF) {
+  outTempC = ((inTempF - 32) * 5/9).toFixed(1);
+  return Number(outTempC);
 };
 
-const ctof = function() {
-
+const ctof = function(inTempC) {
+  outTempF = ((inTempC * 9/5) + 32).toFixed(1);
+  return Number(outTempF);
 };
 
 // Do not edit below this line
