c0abe21ba79f3b2a2c454553879ae4c983d970a9
duraznoj <sashka.warner@gmail.com>
2022-08-29 17:20:02 -0700

    Complete exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (inString) {
 
-};
+  inString = inString.replace(/[^A-Za-z]/g, '').toUpperCase();
+  len = inString.length;
+
+  for(let i = 0; i < len; i++) {
+    if(inString[i] !== inString[len - 1 - i]) {
+      return false;
+    };
+  };
+  return true;
+
+}
 
 // Do not edit below this line
 module.exports = palindromes;
