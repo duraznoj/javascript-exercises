99f16738b3e044cd1fc4d455aa20a3d2c44b52b3
duraznoj <sashka.warner@gmail.com>
2022-08-29 18:07:41 -0700

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+  const sorted = obj.sort((a,b) => {
+   
+    return ((a.yearOfDeath ? a.yearOfDeath : (new Date).getFullYear()) - a.yearOfBirth) > ((b.yearOfDeath ? b.yearOfDeath : (new Date).getFullYear()) - b.yearOfBirth) ? -1 : 1;
+  });
+  //console.table(sorted);
+  return sorted[0];
 };
 
 // Do not edit below this line
